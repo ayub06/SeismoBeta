00f63b002157fd657cb95020d13bf2108bdeaffe
Shalahudin Al Ayyub <ayubalandroid@gmail.com>
2016-06-01 11:12:39 +0700

    Add Picture By Camera

--- app/src/main/java/com/pkm/proyek/seismoalpha/laporan/tim/InputActivity.java
+++ app/src/main/java/com/pkm/proyek/seismoalpha/laporan/tim/InputActivity.java
@@ -2,13 +2,19 @@ package com.pkm.proyek.seismoalpha.laporan.tim;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -40,10 +46,14 @@ public class InputActivity extends AppCompatActivity {
     private static Activity activity;
     public static ProgressBar loading;
 
+    Button upload;
+    ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
 
         activity=this;
         //Loading init
@@ -61,6 +71,15 @@ public class InputActivity extends AppCompatActivity {
 
         inputRusakBerat = (EditText) findViewById(R.id.input_rusak_berat);
         inputRusakRingan = (EditText) findViewById(R.id.input_rusak_ringan);
+
+        mImageView=(ImageView)findViewById(R.id.foto_upload);
+        upload=(Button)findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         assert fab != null;
@@ -104,7 +123,6 @@ public class InputActivity extends AppCompatActivity {
                 new Pair<Context, String>(this, String.valueOf(LaporanActivity.indexGempa))
         );
     }
-
 
     private void ambilDataInput() {
         //Get Location Pelapor
@@ -151,6 +169,20 @@ public class InputActivity extends AppCompatActivity {
         NavUtils.navigateUpFromSameTask(activity);
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
 }
